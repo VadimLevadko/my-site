7280eae8eab3b39484a09f36b0fcbca5cbb6d1c7
VadimLevadko <vadimlevadko@gmail.com>
2022-03-13 22:36:27 +0200

    Added full page for computer, but withot animation

---
+++ projects/antools/js/script.js
@@ -0,0 +1,6 @@
+$('.slider-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    arrows: true,
+    slidesToScroll: 1
+  });
--- projects/aron/js/script.js
+++
@@ -1,6 +0,0 @@
-$('.slider-items').slick({
-    infinite: false,
-    slidesToShow: 3,
-    arrows: false,
-    slidesToScroll: 3
-  });
