024dcca45b54b4131e20a21a0952f26da49ac529
VadimLevadko <vadimlevadko@gmail.com>
2022-03-30 23:06:35 +0300

    Added media but not full

--- projects/antools/js/script.js
+++ projects/antools/js/script.js
@@ -2,5 +2,7 @@ $('.slider-items').slick({
     infinite: true,
     slidesToShow: 1,
     arrows: true,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    prevArrow: $('.arrow-back'),
+    nextArrow: $('.arrow-next')
   });
