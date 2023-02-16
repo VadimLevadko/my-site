c66aecb9b62ccc846b9f45f9ee66d9144c4660e0
VadimLevadko <vadimlevadko@gmail.com>
2023-02-17 00:13:27 +0200

    Aron || Fixed arrows, media and slider

--- projects/aron/js/script.js
+++ projects/aron/js/script.js
@@ -2,5 +2,7 @@ $('.slider-items').slick({
     infinite: false,
     slidesToShow: 3,
     arrows: true,
-    slidesToScroll: 1
-  });
+    slidesToScroll: 1,
+    prevArrow: $('.slick-prev'),
+    nextArrow: $('.slick-next')
+});
