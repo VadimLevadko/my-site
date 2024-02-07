732d983d35305529378303e0e8a8eb0d1e62a068
VadimLevadko <vadimlevadko@gmail.com>
2024-02-07 19:35:20 +0200

    Added media for slider page

--- projects/basterCar/js/script.js
+++ projects/basterCar/js/script.js
@@ -1,18 +1,3 @@
-var swiper = new Swiper(".swiper-container", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 1,
-    depth: 90,
-    modifier: 5.5,
-    slideShadows: true,
-  },
-  loop: true,
-});
-
 const btnDetails = document.querySelectorAll('.btn-details')
 
 btnDetails.forEach(btn => {
